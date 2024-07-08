2a3d950f175172e07bba9a1fe3effa7c5546321e
jaylenchan <jaylen.work@hotmail.com>
2024-07-08 18:33:02 +0800

    fix: 某些界面点击不展示logo

--- config/.vitepress/site-config/head.ts
+++ config/.vitepress/site-config/head.ts
@@ -3,5 +3,5 @@ import { DefaultTheme, LocaleSpecificConfig } from 'vitepress';
 
 export const head: LocaleSpecificConfig<DefaultTheme.Config>['head'] = [
   // 这里配置网站的logo，即网页tab上的logo图案
-  ['link', { rel: 'icon', type: 'image/svg', href: './svgs/avatar.svg' }],
+  ['link', { rel: 'icon', type: 'image/svg', href: '/svgs/avatar.svg' }],
 ]
